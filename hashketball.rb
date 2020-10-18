02bdb4281b9861ed94297d991c1e62ca1985464c
Augie Menos <augie.menos@gmail.com>
2020-10-18 05:29:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ require 'pry'
 def game_hash
   basketball_info = {
     :home => {:team_name => "Brooklyn Nets", :colors => "Black, White",
-      :players => [{:player_name => "Alan Anderson",}, {}, {}, {}, {}]},
+      :players => [{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, {}, {}, {}, {}]},
     :away => {:team_name => "Charlotte Hornets", :colors => "Turquoise, Purple",
       :players => [{}, {}, {}, {}, {}]}
 }
