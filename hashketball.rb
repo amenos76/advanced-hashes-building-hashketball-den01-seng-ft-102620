23850e532e11219eace12763d6ebd9ec5ab54241
Augie Menos <augie.menos@gmail.com>
2020-10-18 05:24:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def game_hash
   basketball_info = {
-    :home => {:team_name => "Brooklyn Nets", :colors => "Black, White", 
+    :home => {:team_name => "Brooklyn Nets", :colors => "Black, White",
       :players =>},
-    :away => {:team_name => "Charlotte Hornets", :colors => "Turquoise, Purple", 
+    :away => {:team_name => "Charlotte Hornets", :colors => "Turquoise, Purple",
       :players =>}
 }
 end
